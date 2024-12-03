533177dd1ff70b8d4d521c483d3a8dffa35c6b35
gitgab29 <gabrieljericholimbo29@gmail.com>
2024-12-03 23:55:44 +0800

    Update users.sql

--- databases/users.sql
+++ databases/users.sql
@@ -1,2 +1,8 @@
-INSERT INTO users (name, email, password, age, gender, weight)
-VALUES ('Test User', 'testuser@example.com', 'password123', 25, 'Male', 70.0);
+-- INSERT INTO users (name, email, password, age, gender, weight)
+-- VALUES ('Test User', 'testuser@example.com', 'password123', 25, 'Male', 70.0);
+
+-- INSERT INTO users (name, email, password, age, gender, weight)
+-- VALUES ('Test User One', 'testuserone@example.com', 'password123one', 26, 'Female', 71.0);
+
+-- INSERT INTO users (name, email, password, age, gender, weight)
+-- VALUES ('Test User Fail', 'testuserone@example.com', 'password123one', 26, 'Female', 71.0);
