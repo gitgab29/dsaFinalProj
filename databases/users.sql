463acc380f96ee68d977e42cae31d87df76e2981
gitgab29 <gabrieljericholimbo29@gmail.com>
2024-12-06 00:03:54 +0800

    Update users.sql

--- databases/users.sql
+++ databases/users.sql
@@ -1,12 +1,12 @@
-    -- INSERT INTO users (name, email, password, age, gender, weight)
-    -- VALUES ('Test User', 'testuser@example.com', 'password123', 25, 'Male', 70.0);
+    -- -- INSERT INTO users (name, email, password, age, gender, weight)
+    -- -- VALUES ('Test User', 'testuser@example.com', 'password123', 25, 'Male', 70.0);
 
-    -- INSERT INTO users (name, email, password, age, gender, weight)
-    -- VALUES ('Test User One', 'testuserone@example.com', 'password123one', 26, 'Female', 71.0);
+    -- -- INSERT INTO users (name, email, password, age, gender, weight)
+    -- -- VALUES ('Test User One', 'testuserone@example.com', 'password123one', 26, 'Female', 71.0);
 
-    -- INSERT INTO users (name, email, password, age, gender, weight)
-    -- VALUES ('Test User Fail', 'testuserone@example.com', 'password123one', 26, 'Female', 71.0);
+    -- -- INSERT INTO users (name, email, password, age, gender, weight)
+    -- -- VALUES ('Test User Fail', 'testuserone@example.com', 'password123one', 26, 'Female', 71.0);
 
-    .mode csv
+    -- .mode csv
     -- CREATE TABLE exercises (activity TEXT, lightweight FLOAT, middleweight FLOAT, cruiserweight FLOAT, heavyweight FLOAT, calories FLOAT);
-    .import  databases\exercise_dataset.csv exercises
+    -- .import  databases/exercise_dataset.csv exercises
